db0dc7174457ddeef478c3e3efd345a3788f2a4d
enyafetizanan <102496233+enyafetizanan@users.noreply.github.com>
2022-04-04 23:26:18 +0800

    navbar final

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const navSlide = () => {
             if(link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 2}s';
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
 
-        //Burger Anmiation
+        //Burger Animation
         burger.classList.toggle('toggle');
     });
 }
